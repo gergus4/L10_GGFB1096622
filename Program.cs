4ebeaedb55933b1662fa0a0a52ddc3290a8e6bc4
gerfu <gerfu@HPGERARDO>
2022-10-14 13:17:32 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    static bool Login(string contra, string usuario)
+    {
+
+        if (contra == "asdasd" && usuario == "usuario1")
+        {
+            return true;
+        }
+        else
+        {
+
+            return false;
+        }
+
+
+    }
+
+    static void Main(string[] args)
+    {
+        string contra;
+        string usuario;
+        int intento = 0;
+
+        bool entrada;
+        do
+        {
+            Console.WriteLine("Bienvenido al inicio de sesión");
+            
+            Console.WriteLine("Ingrese el usuario");
+            usuario = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la contraseña");
+            contra = Console.ReadLine();
+
+            entrada = Login(contra, usuario);
+            if (entrada == true)
+            {
+                Console.WriteLine("Ha entrado con éxito");
+                break;
+            }
+            else
+            {
+                intento++;
+
+                Console.WriteLine("Credenciales inválidas, repita el proceso");
+                Console.WriteLine("Cantidad de intentos: " + intento);
+            }
+        } while (intento < 3);
+        Console.ReadKey();
+    }
+
+}
+
